5e6830740b9174a2bf8b8de056eb28a27b37f472
Ashish <ashishrajstr@gmail.com>
2023-01-13 15:01:10 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,24 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args){
+        Product p = new Product();
+        p.product(10,20);
+        p.product(10,20,30);
+        p.product(10.0,20.0);
+    }
+
+    public static class Product{
+        public static int product(int x, int y) {
+            return x*y;
+        }
+
+        public static int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public static double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
